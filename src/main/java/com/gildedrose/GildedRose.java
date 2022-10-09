5f09e369a9d90b56797b129eadcb70dc2e6d4c1f
YassMk1 <yassine_187@hotmail.com>
2022-10-09 21:53:07 +0200

    updateConjured method

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -20,7 +20,7 @@ class GildedRose {
     		 	case "Sulfuras, Hand of Ragnaros":
     		 		updateSulfuras(item);
     		 		break;
-    		 	case "Conjured":
+    		 	case "Conjured Mana Cake":
     		 		updateConjured(item);
     		 		break;
     		 	default:
@@ -40,9 +40,15 @@ class GildedRose {
     }
 
 	private void updateConjured(Item item) {
-		// TODO Auto-generated method stub
-	}
-
+		if (item.quality > 0) {
+			item.quality = item.quality - 2;
+        }
+		updateSellIn(item);
+		if (item.sellIn < 0 && item.quality > 0) {
+            	item.quality = item.quality - 2;
+            }
+    }
+	
 	private void updateSulfuras(Item item) {
 		// Nothing to do
 	}
@@ -51,14 +57,12 @@ class GildedRose {
 		 if (item.quality < 50) {
              item.quality = item.quality + 1;
              
-             if (item.sellIn < 11) {
-                 if (item.quality < 50) {
+             if (item.sellIn < 11 && item.quality < 50) {
                      item.quality = item.quality + 1;
-                 }
-             } if (item.sellIn < 6) {
-                 if (item.quality < 50) {
+                 
+             } if (item.sellIn < 6 && item.quality < 50) {
                      item.quality = item.quality + 1;
-                 }
+                 
              }
          }
 		 updateSellIn(item);
