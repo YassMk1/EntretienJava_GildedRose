d18127558794afe7afdc3809f414b26e24e0ce34
YassMk1 <yassine_187@hotmail.com>
2022-10-09 21:17:05 +0200

    same as Brie, updateSellIn in Pass & NormalItem method

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -38,7 +38,10 @@ class GildedRose {
 			item.quality = item.quality - 1;
         }
 		updateSellIn(item);
-	}
+		if (item.sellIn < 0 && item.quality > 0) {
+            	item.quality = item.quality - 1;
+            }
+    }
 
 	private void updateConjured(Item item) {
 		// TODO Auto-generated method stub
@@ -63,6 +66,9 @@ class GildedRose {
              }
          }
 		 updateSellIn(item);
+		 if (item.sellIn < 0) {
+			 item.quality = item.quality - item.quality;
+         }
 		
 	}
 
